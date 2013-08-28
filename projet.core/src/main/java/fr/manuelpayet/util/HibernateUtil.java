79327e48bb1c5685a369c59b8e9a281bd746ef7b
Manuel PAYET <manuelpayet@gmail.com>
2013-08-28 19:35:53 +0200

    Ajout de la classe HibernateUtil
    Pas sûr que cette classe soit utile... surtout dans cette forme et dans ce contexte

---
+++ projet.core/src/main/java/fr/manuelpayet/util/HibernateUtil.java
@@ -0,0 +1,32 @@
+package fr.manuelpayet.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HibernateUtil {
+
+  private static Logger log = LoggerFactory.getLogger(HibernateUtil.class);
+
+  private static final SessionFactory sessionFactory = buildSessionFactory();
+
+  @SuppressWarnings("deprecation")
+  private static SessionFactory buildSessionFactory() {
+    log.debug("");
+    try {
+      // Create the SessionFactory from hibernate.cfg.xml
+      return new Configuration().configure().buildSessionFactory();
+    } catch (Throwable ex) {
+      // Make sure you log the exception, as it might be swallowed
+      log.error("Initial SessionFactory creation failed : {}", ex);
+      throw new ExceptionInInitializerError(ex);
+    }
+  }
+
+  public static SessionFactory getSessionFactory() {
+    log.debug("");
+    return sessionFactory;
+  }
+
+}
