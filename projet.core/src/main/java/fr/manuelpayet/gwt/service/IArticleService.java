3ff68035eb25e825bd90a5c9fe01067b807f7e96
Manuel PAYET <manuelpayet@gmail.com>
2013-08-28 17:53:49 +0200

    Ré-Initialisation
    J'ai recréé les projets en utilisant les archetypes :
     - "mvn archetype:generate -DarchetypeGroupId=org.apache.maven.archetypes -DarchetypeArtifactId=maven-archetype-quickstart", pour les projets projet.parent et projet.core
     - "mvn archetype:generate -DarchetypeGroupId=org.codehaus.mojo -DarchetypeArtifactId=gwt-maven-plugin -DarchetypeVersion=2.5.1", pour le projet projet.gwt

--- projet.core/src/main/java/fr/manuelpayet/gwt/service/IArticleService.java
+++
@@ -1,9 +0,0 @@
-package fr.manuelpayet.gwt.service;
-
-import fr.manuelpayet.gwt.core.Article;
-
-public interface IArticleService {
-
-  public Article mergeArticle(Article article);
-
-}
--- projet.gwt/src/main/java/fr/manuelpayet/gwt/client/rpc/ArticleServiceRPCAsync.java
+++
@@ -1,9 +0,0 @@
-package fr.manuelpayet.gwt.client.rpc;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-import fr.manuelpayet.gwt.core.Article;
-
-public interface ArticleServiceRPCAsync {
-  public void mergeArticle(Article article, AsyncCallback<Article> callback);
-}
