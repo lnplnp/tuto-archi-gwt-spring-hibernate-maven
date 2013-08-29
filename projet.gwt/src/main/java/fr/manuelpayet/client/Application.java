59d3076a1c2366fcc49feb0c478dc37310c2e81e
Manuel PAYET <manuelpayet@gmail.com>
2013-08-29 15:27:01 +0200

    Changement de nom de package pour l'objet Article

--- projet.gwt/src/main/java/fr/manuelpayet/client/Application.java
+++ projet.gwt/src/main/java/fr/manuelpayet/client/Application.java
@@ -12,41 +12,42 @@ import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 
 import fr.manuelpayet.gwt.client.rpc.ArticleServiceRPC;
-import fr.manuelpayet.gwt.core.Article;
+import fr.manuelpayet.gwt.core.model.Article;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class Application implements EntryPoint {
   public void onModuleLoad() {
-    //composants
+    // composants
     Label label = new Label("libellé de l'article");
     final TextBox textbox = new TextBox();
     Button button = new Button("valider");
 
-    //action sur le bouton
+    // action sur le bouton
     button.addClickHandler(new ClickHandler() {
-        public void onClick(ClickEvent event) {
-            final AsyncCallback<Article> callback = new AsyncCallback<Article>(){
-                public void onFailure(Throwable err) {
-                    Window.alert("Failure");
-                }
-                public void onSuccess(Article result) {
-                    Window.alert("Success : " + result.getLibelleArticle());
-                }
-            };
-            //creation d'une instance article
-            Article article = new Article(textbox.getText());
-            //appel rpc
-            ArticleServiceRPC.Util.getInstance().mergeArticle(article, callback);
-        }
+      public void onClick(ClickEvent event) {
+        final AsyncCallback<Article> callback = new AsyncCallback<Article>() {
+          public void onFailure(Throwable err) {
+            Window.alert("Failure");
+          }
+
+          public void onSuccess(Article result) {
+            Window.alert("Success : " + result.getLibelleArticle());
+          }
+        };
+        // creation d'une instance article
+        Article article = new Article(textbox.getText());
+        // appel rpc
+        ArticleServiceRPC.Util.getInstance().mergeArticle(article, callback);
+      }
     });
-    //panel
+    // panel
     HorizontalPanel panel = new HorizontalPanel();
     panel.add(label);
     panel.add(textbox);
     panel.add(button);
     RootPanel.get().add(panel);
-}
+  }
 
 }
